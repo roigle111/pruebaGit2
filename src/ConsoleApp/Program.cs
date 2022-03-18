d64c74b00c910d81a1e39b9a030d7093c29e3790
rodrigo ledesma <rodrigoiledesma@gmail.com>
2022-03-18 14:08:01 -0300

    Primer subida de archivso al repository

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersistenceDatabase;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionBuilder.UseSqlServer(@"data source=localhost\SQLEXPRESS;initial catalog=EFPrueba2;User ID=SisDialysisPub;Password=S1sD1alys1s@@733.;MultipleActiveResultSets=True;Application Name=EntityFramework");
+
+            var context = new ApplicationDbContext(optionBuilder.Options);
+            TestConnection(context);
+        }
+
+        static void TestConnection(ApplicationDbContext context)
+        {
+            var isConnect = false;
+            try
+            {
+                isConnect = context.Database.EnsureCreated();
+            }
+            catch (System.Exception)
+            {
+                //throw;
+            }
+
+            if (isConnect)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("SE ESTABLECIO LA CONEXION EXITOSA");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("NO SE PUDO ESTABLECER LA CONEXION");
+            }
+        }
+    }
+}
---
+++ src/PersistenceDatabase/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersistenceDatabase
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
