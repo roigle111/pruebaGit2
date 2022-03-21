82da36354db850702290cbb5a2de9883ac3a5e38
rodrigo ledesma <rodrigoiledesma@gmail.com>
2022-03-21 16:06:08 -0300

    ServiceClients

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,6 +1,8 @@
-﻿using Common;
+﻿using BetterConsoleTables;
+using Common;
 using Microsoft.EntityFrameworkCore;
 using PersistenceDatabase;
+using Services;
 using System;
 
 namespace ConsoleApp
@@ -13,7 +15,32 @@ namespace ConsoleApp
             optionBuilder.UseSqlServer(Parameter.ConectionString);
 
             var context = new ApplicationDbContext(optionBuilder.Options);
-            TestConnection(context);
+            //TestConnection(context);
+
+            var clientService = new ClientService(context);
+
+
+            using (context)
+            {
+                PrintClientTable(clientService);
+            }
+
+            Console.Read();
+        }
+
+        static void PrintClientTable(ClientService clientService)
+        {
+            var clients = clientService.GetAll();
+
+            // BetterConsoleTables (1.1.2)
+            var table = new Table("ClientId", "ClientNumber", "Name", "Country");
+
+            foreach (var client in clients)
+            {
+                table.AddRow(client.ClientId, client.ClientNumber, client.Name, client.Country?.Name ?? "-");
+                Console.Write(table.ToString());
+            }
+
         }
 
         static void TestConnection(ApplicationDbContext context)
---
+++ src/Services/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using PersistenceDatabase;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+    public class ClientService
+    {
+        private readonly ApplicationDbContext _context;
+        public ClientService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Client> GetAll()
+        {
+            return _context.Clients.ToList();
+        }
+
+    }
+}
